46f2a3d3dcac00a7db385b351032d67afac2df73
AdithyanPadmanabhan <197160@ust.com>
2023-12-12 15:33:35 +0530

    12-12-23  Assignment

---
+++ AssignmentNunit/UserData.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentNunit
+{
+    public class UserData
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+
+        [JsonProperty("userId")]
+        public int UserId { get; set; }
+
+
+        [JsonProperty("title")]
+        public string? Title { get; set; }
+
+
+        [JsonProperty("body")]
+        public string? Body { get; set; }
+
+       
+
+    }
+    
+}
