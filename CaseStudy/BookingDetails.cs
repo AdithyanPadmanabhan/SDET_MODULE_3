08488ea4164ea276cf2a8fe60928f4af33c545ea
AdithyanPadmanabhan <197160@ust.com>
2023-12-14 16:56:21 +0530

    CaseStudy 14-12-23

---
+++ CaseStudy/BookingDetails.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseStudy
+{
+    public class BookingDetails
+    {
+
+        [JsonProperty("firstname")]
+        public string? FirstName { get; set; }
+
+
+        [JsonProperty("lastname")]
+        public string? LastName { get; set; }
+
+
+        [JsonProperty("totalprice")]
+        public int TotalPrice { get; set; }
+
+        [JsonProperty("depositpaid")]
+        public bool DepositPaid { get; set; }
+
+
+        [JsonProperty("bookingdates")]
+        public BookingDatesDetails? BookingDates { get; set; }
+
+        
+
+             [JsonProperty("additionalneeds")]
+           public string? AdditionalNeeds { get; set; }
+
+
+
+        [JsonProperty("token")]
+        public string? Token { get; set; }
+
+
+    }
+}
